1145f5d351502834e9a31522a0be96977c55f2ee
dongHyo <kdhyo98@gmail.com>
2022-04-27 23:22:54 +0900

    feat: Groovy Gradle -> Kotlin Gradle DSL
    Kotlin Gradle DSL 으로 마이그레이션

---
+++ server/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	id ("org.springframework.boot") version "2.6.7"
+	id ("io.spring.dependency-management") version "1.0.11.RELEASE"
+}
+
+group = "com.ticketing"
+version = "0.0.1-SNAPSHOT"
+
+val javaVersion = JavaVersion.VERSION_11
+java {
+	sourceCompatibility = javaVersion
+	targetCompatibility = javaVersion
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation ("org.springframework.boot:spring-boot-starter-security")
+	implementation ("org.springframework.boot:spring-boot-starter-validation")
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+
+	compileOnly ("org.projectlombok:lombok")
+	runtimeOnly ("mysql:mysql-connector-java")
+	annotationProcessor ("org.projectlombok:lombok")
+
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	testImplementation ("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
