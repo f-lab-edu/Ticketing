08ce8e690403417803bc248ece01a4d38e440edd
dongHyo <kdhyo98@gmail.com>
2022-05-06 13:48:46 +0900

    refactor: @Component 등록

--- server/src/main/java/com/ticketing/server/global/health/ManualHealthIndicator.java
+++ server/src/main/java/com/ticketing/server/global/health/ManualHealthIndicator.java
@@ -2,7 +2,9 @@ package com.ticketing.server.global.health;
 
 import java.util.concurrent.atomic.AtomicReference;
 import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ManualHealthIndicator implements MutableHealthIndicator {
 
 	private final AtomicReference<Health> healthRef = new AtomicReference<>(Health.up().build());
