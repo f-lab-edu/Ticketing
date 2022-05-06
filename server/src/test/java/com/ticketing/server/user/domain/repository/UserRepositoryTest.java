60ff034171b38c05abec7578048f20f0916b9db5
dongHyo <kdhyo98@gmail.com>
2022-05-07 01:22:40 +0900

    refactor: @Transactional 추가

--- server/src/test/java/com/ticketing/server/user/domain/repository/UserRepositoryTest.java
+++ server/src/test/java/com/ticketing/server/user/domain/repository/UserRepositoryTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class UserRepositoryTest {
 
 	@Autowired
