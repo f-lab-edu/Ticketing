15f93eec52c96876b5aef316d35438b3b97e1979
dongHyo <kdhyo98@gmail.com>
2022-05-07 01:29:15 +0900

    refactor: import 정리 및 하단 공백 추가

--- server/src/test/java/com/ticketing/server/user/domain/repository/UserRepositoryTest.java
+++ server/src/test/java/com/ticketing/server/user/domain/repository/UserRepositoryTest.java
@@ -6,7 +6,6 @@ import com.ticketing.server.user.domain.User;
 import com.ticketing.server.user.domain.UserGrade;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
