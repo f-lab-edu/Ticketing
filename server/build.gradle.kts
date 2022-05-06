1dca969907ac76c7246e19a5fe0bb017d3d94dae
dongHyo <kdhyo98@gmail.com>
2022-05-07 00:32:28 +0900

    feat: log4j2 설정

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -23,14 +23,23 @@ repositories {
 	mavenCentral()
 }
 
+
+
 dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation ("org.springframework.boot:spring-boot-starter-security")
 	implementation ("org.springframework.boot:spring-boot-starter-validation")
 	implementation ("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-
 	implementation ("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
+	implementation ("org.springframework.boot:spring-boot-starter-log4j2")
+	implementation ("com.lmax:disruptor:3.4.2")
+
+	modules {
+		module("org.springframework.boot:spring-boot-starter-logging") {
+			replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+		}
+	}
 
 	compileOnly ("org.projectlombok:lombok")
 	runtimeOnly ("mysql:mysql-connector-java")
