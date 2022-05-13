1138860305a041f084a28770c3642e73e6c3d9ba
dongHyo <kdhyo98@gmail.com>
2022-05-13 17:06:44 +0900

    feat: validation annotation 변경 및 @Builder 제거

--- server/src/main/java/com/ticketing/server/user/domain/User.java
+++ server/src/main/java/com/ticketing/server/user/domain/User.java
@@ -1,12 +1,15 @@
 package com.ticketing.server.user.domain;
 
 import com.ticketing.server.global.dto.repository.AbstractEntity;
+import com.ticketing.server.global.validator.constraints.Phone;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,28 +18,34 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User extends AbstractEntity {
 
-	@NotNull
+	@Column(name = "name")
+	@NotEmpty(message = "이름은 필수 입니다.")
 	private String name;
 
-	@NotNull
+	@Column(name = "email")
+	@NotEmpty(message = "이메일은 필수 입니다.")
+	@Email(message = "이메일이 올바르지 않습니다.")
 	private String email;
 
-	@NotNull
+	@Column(name = "password")
+	@NotEmpty(message = "패스워드는 필수 입니다.")
 	private String password;
 
+	@Column(name = "grade")
 	@NotNull
 	@Enumerated(value = EnumType.STRING)
 	private UserGrade grade;
 
-	@NotNull
+	@Column(name = "phone")
+	@NotEmpty(message = "휴대번호는 필수 입니다.")
+	@Phone(message = "휴대번호가 올바르지 않습니다.")
 	private String phone;
 
 	private boolean isDeleted = false;
 
 	private LocalDateTime deletedAt;
 
-	@Builder
-	protected User(String name, String email, String password, UserGrade grade, String phone) {
+	public User(String name, String email, String password, UserGrade grade, String phone) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
