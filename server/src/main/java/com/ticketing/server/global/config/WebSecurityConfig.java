b60f641b1d548499c46175ebc8ced83b005476c3
dongHyo <kdhyo98@gmail.com>
2022-05-16 23:59:14 +0900

    feat: 시큐리티 disable 및 psswordEncoder 빈등록

---
+++ server/src/main/java/com/ticketing/server/global/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ticketing.server.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.cors().disable()
+			.csrf().disable()
+			.formLogin().disable()
+			.headers().frameOptions().disable();
+	}
+
+}
