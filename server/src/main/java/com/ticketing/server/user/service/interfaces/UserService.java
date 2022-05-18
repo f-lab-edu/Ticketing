a1a4f39d19a44aa50c0f669a3e6164b4d8d311fa
dongHyo <kdhyo98@gmail.com>
2022-05-19 01:36:44 +0900

    fix: service 구현체와 동일하게 파라미터 수정

--- server/src/main/java/com/ticketing/server/user/service/interfaces/UserService.java
+++ server/src/main/java/com/ticketing/server/user/service/interfaces/UserService.java
@@ -2,9 +2,10 @@ package com.ticketing.server.user.service.interfaces;
 
 import com.ticketing.server.user.domain.User;
 import com.ticketing.server.user.service.dto.SignUp;
+import javax.validation.Valid;
 
 public interface UserService {
 
-	User register(SignUp signUpDto);
+	User register(@Valid SignUp signUpDto);
 
 }
