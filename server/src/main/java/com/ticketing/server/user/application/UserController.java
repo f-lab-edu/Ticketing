ba79691b4b45405c15ab50a31c040a5cc8b83ef1
dongHyo <kdhyo98@gmail.com>
2022-05-22 22:05:38 +0900

    feat: 패스워드 변경 컨트롤러 구현

--- server/src/main/java/com/ticketing/server/user/application/UserController.java
+++ server/src/main/java/com/ticketing/server/user/application/UserController.java
@@ -2,6 +2,7 @@ package com.ticketing.server.user.application;
 
 import com.ticketing.server.user.application.request.SignUpRequest;
 import com.ticketing.server.user.application.request.UserDeleteRequest;
+import com.ticketing.server.user.application.request.UserPasswordModifyRequest;
 import com.ticketing.server.user.domain.User;
 import com.ticketing.server.user.service.UserServiceImpl;
 import java.util.Optional;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +40,21 @@ public class UserController {
 	@DeleteMapping
 	public ResponseEntity<Object> delete(@RequestBody @Valid UserDeleteRequest userDeleteRequest) {
 		Optional<User> user = userService.delete(userDeleteRequest.toDeleteUser(passwordEncoder));
+
+		if (user.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	@PatchMapping("/password")
+	public ResponseEntity<Object> modifyPassword(@RequestBody @Valid UserPasswordModifyRequest request) {
+		if (request.oldEqualNew()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+		Optional<User> user = userService.modifyPassword(request.toChangePassword(passwordEncoder));
 
 		if (user.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
--- server/src/main/java/com/ticketing/server/user/application/request/UserPasswordModifyRequest.java
+++ server/src/main/java/com/ticketing/server/user/application/request/UserPasswordModifyRequest.java
@@ -1,2 +1,27 @@
-package com.ticketing.server.user.application.request;public class UserPasswordModifyRequset {
+package com.ticketing.server.user.application.request;
+
+import com.ticketing.server.user.service.dto.ChangePassword;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserPasswordModifyRequest {
+
+	@NotEmpty(message = "{validation.not.empty.email}")
+	@Email(message = "{validation.email}")
+	private String email;
+
+	@NotEmpty(message = "{validation.not.empty.oldpassword}")
+	private String oldPassword;
+
+	@NotEmpty(message = "{validation.not.empty.newpassword}")
+	private String newPassword;
+
+	public ChangePassword toChangePassword(PasswordEncoder passwordEncoder) {
+		return new ChangePassword(email, oldPassword, newPassword, passwordEncoder);
+	}
+
+	public boolean oldEqualNew() {
+		return oldPassword.equals(newPassword);
+	}
 }
