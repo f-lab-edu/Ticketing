b804c2bf257cbc014388094d5979de679800c779
dongHyo <kdhyo98@gmail.com>
2022-05-31 16:11:02 +0900

    feat: configuration-processor 추가

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
