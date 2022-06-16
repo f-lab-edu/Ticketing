56b926b8271de4b1a03f75774b4e63352a98155e
손창현 <gganzii1215@gmail.com>
2022-06-16 20:14:24 +0900

    refactor: MovieDto 필드에 @JsonProperty 추가

--- server/src/main/java/com/ticketing/server/movie/application/response/MovieListResponse.java
+++ server/src/main/java/com/ticketing/server/movie/application/response/MovieListResponse.java
@@ -1,5 +1,6 @@
 package com.ticketing.server.movie.application.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ticketing.server.movie.service.dto.MovieDto;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
@@ -12,6 +13,7 @@ import lombok.Getter;
 public class MovieListResponse {
 
     @ApiModelProperty(value = "영화 제목")
+    @JsonProperty
     private List<MovieDto> movieDtos;
 
     public static MovieListResponse from(List<MovieDto> movieDtos) {
--- server/src/main/java/com/ticketing/server/movie/service/dto/MovieDto.java
+++ server/src/main/java/com/ticketing/server/movie/service/dto/MovieDto.java
@@ -1,10 +1,12 @@
 package com.ticketing.server.movie.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class MovieDto {
 
+    @JsonProperty
     private String title;
 
 }
