d0af7e44442bc767895bbdf3b8d60520320fdb87
Kim DongHyo <60608509+kdhyo@users.noreply.github.com>
2022-06-30 16:31:54 +0900

    feat: 로그아웃 시 refreshToken 제거 (#44)
    * feat: 로그아웃 시 refreshToken 제거
    
    * refactor: deleteRefreshToken 메서드 람다활용 문법으로 변경

--- server/src/main/java/com/ticketing/server/user/service/interfaces/AuthenticationService.java
+++ server/src/main/java/com/ticketing/server/user/service/interfaces/AuthenticationService.java
@@ -1,5 +1,6 @@
 package com.ticketing.server.user.service.interfaces;
 
+import com.ticketing.server.user.application.response.LogoutResponse;
 import com.ticketing.server.user.application.response.TokenDto;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
@@ -8,5 +9,7 @@ public interface AuthenticationService {
 	TokenDto generateTokenDto(UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken);
 
 	TokenDto reissueTokenDto(String bearerRefreshToken);
+
+	LogoutResponse deleteRefreshToken(String email);
 
 }
