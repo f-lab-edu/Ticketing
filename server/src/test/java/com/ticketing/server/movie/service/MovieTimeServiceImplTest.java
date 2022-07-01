c807e715238c036b448d8b64e4c167eff82a2269
손창현 <gganzii1215@gmail.com>
2022-07-02 03:10:14 +0900

    refactor: removed unnecessary imports

--- server/src/test/java/com/ticketing/server/movie/service/MovieTimeServiceImplTest.java
+++ server/src/test/java/com/ticketing/server/movie/service/MovieTimeServiceImplTest.java
@@ -1,18 +1,8 @@
 package com.ticketing.server.movie.service;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-
-import com.ticketing.server.movie.domain.Movie;
-import com.ticketing.server.movie.domain.repository.MovieRepository;
 import com.ticketing.server.movie.domain.repository.MovieTimeRepository;
-import com.ticketing.server.movie.service.dto.MovieTimeDto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import javax.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,35 +18,16 @@ public class MovieTimeServiceImplTest {
     LocalDateTime endOfDay = startOfDay.plusDays(1);
 
     @Mock
-    MovieRepository movieRepository;
-
-    @Mock
     MovieTimeRepository movieTimeRepository;
-
-    @InjectMocks
-    MovieServiceImpl movieService;
 
     @InjectMocks
     MovieTimeServiceImpl movieTimeService;
 
     @Test
-    @Transactional
+    @Rollback
     @DisplayName("MovieTime Service Test - get empty list when there is no valid movie times")
     void shouldGetEmptyList() {
-        // given
-        String title = "범죄도시2";
-        Movie movie = new Movie(title, 106);
-        when(movieRepository.findByTitle(title)).thenReturn(Optional.of(movie));
 
-        when(movieTimeRepository.findValidMovieTimes(1L, startOfDay, endOfDay))
-            .thenReturn(Collections.emptyList());
-
-        // when
-        Optional<Movie> result = movieRepository.findByTitle(title);
-        List<MovieTimeDto> movieTimeDtoList = movieTimeService.getMovieTimes(title, LocalDate.now());
-
-        // then
-        assertTrue(movieTimeDtoList.isEmpty());
     }
 
 }
