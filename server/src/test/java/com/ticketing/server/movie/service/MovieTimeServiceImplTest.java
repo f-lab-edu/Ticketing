096b1dadf3bbb82d8b59227b0755312d9240c6ba
손창현 <gganzii1215@gmail.com>
2022-07-04 01:47:19 +0900

    feat: MovieTimeServiceImplTest - get movie times 케이스 작성

--- server/src/test/java/com/ticketing/server/movie/service/MovieTimeServiceImplTest.java
+++ server/src/test/java/com/ticketing/server/movie/service/MovieTimeServiceImplTest.java
@@ -1,8 +1,18 @@
 package com.ticketing.server.movie.service;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+
+import com.ticketing.server.movie.domain.Movie;
+import com.ticketing.server.movie.domain.repository.MovieRepository;
 import com.ticketing.server.movie.domain.repository.MovieTimeRepository;
+import com.ticketing.server.movie.service.dto.MovieTimeDto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +28,9 @@ public class MovieTimeServiceImplTest {
     LocalDateTime endOfDay = startOfDay.plusDays(1);
 
     @Mock
+    MovieRepository movieRepository;
+
+    @Mock
     MovieTimeRepository movieTimeRepository;
 
     @InjectMocks
@@ -27,6 +40,18 @@ public class MovieTimeServiceImplTest {
     @Rollback
     @DisplayName("MovieTime Service Test - get empty list when there is no valid movie times")
     void shouldGetEmptyList() {
+        String title = "범죄도시2";
+        Movie movie = new Movie(title, 106);
+
+        when(movieRepository.findByTitle(title)).thenReturn(Optional.of(movie));
+        when(movieTimeRepository.findValidMovieTimes(any(), any(), any()))
+            .thenReturn(Collections.emptyList());
+
+        // when
+        List<MovieTimeDto> movieTimeDtoList = movieTimeService.getMovieTimes(title, LocalDate.now());
+
+        // then
+        assertTrue(movieTimeDtoList.isEmpty());
 
     }
 
