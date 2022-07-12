1d1485c69a2a82c17e3de92f041b684c07a5b456
손창현 <gganzii1215@gmail.com>
2022-07-13 01:19:17 +0900

    add: findValidMovieWithTitle() in MovieRepository

--- server/src/main/java/com/ticketing/server/movie/domain/repository/MovieRepository.java
+++ server/src/main/java/com/ticketing/server/movie/domain/repository/MovieRepository.java
@@ -13,6 +13,12 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     Optional<Movie> findByTitle(String title);
 
+	@Query(value = "SELECT m "
+		+ "FROM movie m "
+		+ "WHERE title = :title "
+		+ "AND deleted_at IS NULL")
+	Optional<Movie> findValidMovieWithTitle(String title);
+
     @Query(value = "SELECT * "
         + "FROM movie "
         + "WHERE deleted_at IS NULL", nativeQuery = true)
