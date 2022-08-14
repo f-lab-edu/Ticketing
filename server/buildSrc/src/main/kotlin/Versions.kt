a14020dfb59bb05ec16b35e652de3d4d1ff6b8a8
dongHyo <kdhyo98@gmail.com>
2022-08-14 16:10:04 +0900

    refactor: buildSrc를 통한 의존성 관리

---
+++ server/buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,23 @@
+object Versions {
+
+	const val SPRING_BOOT_STARTER = "2.6.7"
+	const val SPRING_DEPENDENCY_MANAGEMENT = "1.0.11.RELEASE"
+
+	const val SPRING_CLOUD_STARTER = "3.1.3"
+	const val SPRING_CLOUD = "2021.0.3"
+
+	const val MYSQL_CONNECTOR_JAVA = "8.0.28"
+
+	const val LOMBOK = "1.18.24"
+	const val SPRING_FOX = "3.0.0"
+	const val JASYPT = "3.0.4"
+	const val DISRUPTOR = "3.4.4"
+	const val JJWT = "0.11.5"
+	const val JSON_SIMPLE = "1.1.1"
+	const val MICROMETER = "1.8.5"
+	const val JSR350 = "3.0.2"
+
+	const val JUPITER = "5.8.1"
+	const val SECURITY = "5.6.3"
+
+}
